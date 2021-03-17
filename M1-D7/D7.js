d7302d7da5b0a4a0d05dd7beb2a40400171e816d
Saidev Lakkakula <lakkakula.saidev@gmail.com>
2021-03-17 16:35:33 +0100

    This is the list of Tasks

--- M1-D7/D7.js
+++ M1-D7/D7.js
@@ -6,12 +6,18 @@ const changeTitle = function (newTitle) {
   pageTitle = document.getElementsByTagName('h1')[0]
   pageTitle.innerHTML = newTitle
   console.log(pageTitle)
-}; changeTitle('Strive school for strivers')/* EXERCISE 2
+}; 
+
+//changeTitle('Strive school for strivers')
+
+/* EXERCISE 2
 Write a function for changing the class of the title of the page in "myHeading".
 */const addClassToTitle = function () {
   titleData = document.getElementsByTagName('h1')[0]
   titleData.classList.add('myHeading')
-}; addClassToTitle()
+};
+
+addClassToTitle()
 
 /* EXERCISE 3
 Write a function for changing the text of only the p which are children of a div.
@@ -54,11 +60,13 @@ const addToTheSecond = function (content) {
 Write a function for adding a second paragraph to the div.
 */
 const addParagraph = function (content) {
+  
 
-  let newPara = document.createElement('p')
-  let paraText = document.createTextNode(content)
-  newPara.appendChild(paraText)
-  document.getElementsByTagName('div')[0].appendChild(newPara)
+  // let newPara = document.createElement('p')
+  // let paraText = document.createTextNode(content)
+  // newPara.appendChild(paraText)
+  const parent =  document.getElementsByTagName('div')[0]
+  parent.innerHTML += `<p>${content}</p>`
 };
 
 //addParagraph('Hello this is the new paragraph')
@@ -97,7 +105,7 @@ function getRandomColor() {
 function colorChange() {
 
   let headingData = document.querySelector(h1)
-  headingData.style.backgroundColor = getRandomColor()
+  headingData.style.color = getRandomColor()
 }
 
 
@@ -106,29 +114,58 @@ Make the heading of the page change color every time the user clicks on it.
 */
 const makeItClickable = function () {
 
-  let headingData = document.querySelector(h1)
+  let headingData = document.querySelector('h1')
 
-  headingData.setAttribute("onclick", "colorChange()")
+  //headingData.addEventListener('click', function(event){
+
+    //event.target.style.color = getRandomColor()
+  //})
 };
+
+makeItClickable()
 /* EXERCISE 10
 Change the footer text with something else when the user clicks on it.
 */
-const changeFooterText = function () {
+const changeFooterText = function (Text) {
 
+  let oldText
+  let newText = Text
+  const footerChange = document.getElementsByTagName('footer')[0]
 
+  footerChange.onclick = textChanger
 
+  function textChanger(){
+
+    oldText = footerChange.innerText
+    footerChange.innerText = newText
+    changeFooterText(oldText)
+
+    
+  }
 };
+
+changeFooterText('This is the changed footer TEXT!!!')
 /* EXERCISE 11
 Attach an event listener to the input field in the page for console logging its value just after any keystroke.
 */
-const inputField = document.getElementById("input-field");
+const inputField = function(){
+
+  const inputField = document.getElementById("input-field");
+
+  inputField.addEventListener('keypress', function(event){ 
+
+    console.log(event.key)
+  })
+}
+
+inputField()
 // ...
 /* EXERCISE 12
 Create a new welcome alert message when the page successfully loads.
 */
-window.onload = function () {
-  //
-};
+//window.onload = function () {
+  //alert('The page is loaded!!!')
+//};
     /* EXERCISE 13
   Use HTML5 tags to divide the content of the page in a more semantic way.
   */
